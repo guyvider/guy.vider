de40b78e7e6977b285d95899acda9608b1d9662a
guyvider <88592539+guyvider@users.noreply.github.com>
2021-09-13 10:26:13 +0300

    Add files via upload

---
+++ Guy_vider_app/public/javascripts/SeasonServer.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+http.createServer(function (req, res) {
+var q = url.parse(req.url, true);
+var filename = "." + q.pathname;
+fs.readFile(filename, function(err, data) {
+if (err) {
+res.writeHead(404, {'ContentType': 'text/html'});
+return res.end("404 Not Found");
+}
+res.writeHead(200, {'ContentType': 'text/html'});
+res.write(data);
+return res.end();
+});
+}).listen(8080);
---
+++ Guy_vider_app/public/javascripts/cv.js
@@ -0,0 +1,4 @@
+function Function1(){
+    document.getElementById("h1").innerHTML = "Guy Vider";
+    document.getElementById("title").innerHTML = "Guy Vider CV";
+}   
